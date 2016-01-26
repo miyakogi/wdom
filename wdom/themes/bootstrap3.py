cae8fa110231bddbc42a0d9ccd7f4ef0a9e9dfa5
miyakogi <miyako.dev@gmail.com>
2016-01-26 17:40:07 +0900

    Update bootstrap

--- wdom/themes/bootstrap3.py
+++ wdom/themes/bootstrap3.py
@@ -16,22 +16,34 @@ js_files = [
 
 headers = []
 
-NewNode = NewTagClass
+NewTag = NewTagClass
 
-Button = NewNode('Button', bases=Button, class_='btn')
-DefaultButton = NewNode('DefaultButton', bases=Button, class_='btn-default')
-PrimaryButton = NewNode('PrimaryButton', bases=Button, class_='btn-primary')
-SuccessButton = NewNode('SuccessButton', bases=Button, class_='btn-success')
-InfoButton = NewNode('InfoButton', bases=Button, class_='btn-info')
-WarningButton = NewNode('WarningButton', bases=Button, class_='btn-warning')
-DangerButton = NewNode('DangerButton', bases=Button, class_='btn-danger')
-LinkButton = NewNode('LinkButton', bases=Button, class_='btn-link')
+Button = NewTag('Button', bases=Button, class_='btn')
+DefaultButton = NewTag('DefaultButton', 'button', Button, class_='btn-default')
+PrimaryButton = NewTag('PrimaryButton', 'button', Button, class_='btn-primary')
+SuccessButton = NewTag('SuccessButton', 'button', Button, class_='btn-success')
+InfoButton = NewTag('InfoButton', 'button', Button, class_='btn-info')
+WarningButton = NewTag('WarningButton', 'button', Button, class_='btn-warning')
+DangerButton = NewTag('DangerButton', 'button', Button, class_='btn-danger')
+LinkButton = NewTag('LinkButton', 'button', Button, class_='btn-link')
 
-FormGroup = NewNode('FormGroup', bases=Div, class_='form-group')
-TextInput = NewNode('TextInput', bases=TextInput, class_='form-control')
-TextArea = NewNode('TextArea', bases=TextArea, class_='form-control')
-Select = NewNode('Select', bases=Select, class_='form-control')
+FormGroup = NewTag('FormGroup', 'div', class_='form-group')
+TextInput = NewTag('TextInput', 'input', TextInput, class_='form-control')
+TextArea = NewTag('TextArea', 'textarea', TextArea, class_='form-control')
+Select = NewTag('Select', 'select', Select, class_='form-control')
 
-Container = NewNode('Container', bases=Div, class_='container')
+Container = NewTag('Container', 'div', class_='container')
 Wrapper = Container
-Row = NewNode('Row', bases=Div, class_='row')
+Row = NewTag('Row', 'div', class_='row')
+
+Col1 = NewTag('Col', 'div', class_='col-xs-1 col-sm-1 col-md-1 col-lg-1')
+Col2 = NewTag('Col', 'div', class_='col-xs-2 col-sm-2 col-md-2 col-lg-2')
+Col3 = NewTag('Col', 'div', class_='col-xs-3 col-sm-3 col-md-3 col-lg-3')
+Col4 = NewTag('Col', 'div', class_='col-xs-4 col-sm-4 col-md-4 col-lg-4')
+Col5 = NewTag('Col', 'div', class_='col-xs-5 col-sm-5 col-md-5 col-lg-5')
+Col6 = NewTag('Col', 'div', class_='col-xs-6 col-sm-6 col-md-6 col-lg-6')
+Col7 = NewTag('Col', 'div', class_='col-xs-7 col-sm-7 col-md-7 col-lg-7')
+Col8 = NewTag('Col', 'div', class_='col-xs-8 col-sm-8 col-md-8 col-lg-8')
+Col9 = NewTag('Col', 'div', class_='col-xs-9 col-sm-9 col-md-9 col-lg-9')
+Col10 = NewTag('Col', 'div', class_='col-xs-10 col-sm-10 col-md-10 col-lg-10')
+Col11 = NewTag('Col', 'div', class_='col-xs-11 col-sm-11 col-md-11 col-lg-11')
