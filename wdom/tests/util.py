17cc33726bb66f5b5e9c28c7edf93f6ee4720b46
miyakogi <miyako.dev@gmail.com>
2016-01-27 17:53:58 +0900

    Add assertMatch method for test

--- wdom/tests/util.py
+++ wdom/tests/util.py
@@ -6,6 +6,7 @@
 '''
 
 import time
+import re
 import asyncio
 from multiprocessing import Process, Pipe
 import unittest
@@ -27,11 +28,17 @@ browser = None
 
 
 class TestCase(unittest.TestCase):
-    def assertIsTrue(self, bl) -> None:
+    def assertIsTrue(self, bl):
         self.assertIs(bl, True)
 
-    def assertIsFalse(self, bl) -> None:
+    def assertIsFalse(self, bl):
         self.assertIs(bl, False)
+
+    def assertMatch(self, pattern, string):
+        self.assertIsNotNone(re.match(pattern, string))
+
+    def assertNotMatch(self, pattern, string):
+        self.assertIsNone(re.match(pattern, string))
 
 
 def install_asyncio():
