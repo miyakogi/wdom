b1df783030e6a23a3e8a6eaa4e74dd666196f38b
miyakogi <miyako.dev@gmail.com>
2016-03-28 20:51:14 +0900

    Simplify conftest on web test

--- wdom/tests/web/conftest.py
+++ wdom/tests/web/conftest.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sys
-from os import path
-
 import pytest
 
-sys.path.append(path.dirname(path.dirname(path.dirname(path.dirname(__file__)))))
-
 from wdom.tests.web.remote_browser import start_browser, close_browser
+
 
 @pytest.fixture(scope='session', autouse=True)
 def browser(request):
