957ac35f28411f7039b796eb1720dc543ef00666
miyakogi <miyako.dev@gmail.com>
2016-03-31 16:11:36 +0900

    Add server test for tornado

---
+++ wdom/tests/test_server.py
@@ -0,0 +1,91 @@
+
+
+import sys
+from os import path
+import time
+import subprocess
+import logging
+import asyncio
+
+from syncer import sync
+
+from selenium.webdriver.common.utils import free_port
+from tornado import websocket
+from tornado.ioloop import IOLoop
+from tornado.platform.asyncio import AsyncIOMainLoop, to_asyncio_future
+
+from wdom.log import configure_logger
+from wdom.tests.util import TestCase
+
+
+curdir = path.dirname(__file__)
+
+
+def setUpModule():
+    configure_logger(logging.DEBUG)
+    if not IOLoop.initialized():
+        AsyncIOMainLoop().install()
+
+
+class TestAutoShutdownAIO(TestCase):
+    test_file = path.join(curdir, 'aio_server.py')
+
+    def setUp(self):
+        self.port = free_port()
+        cmd = [sys.executable, self.test_file, '--port', str(self.port),
+               '--autoshutdown', '--shutdown-wait', '0.2']
+        self.addr = 'localhost:{}'.format(self.port)
+        self.proc = subprocess.Popen(
+            cmd, cwd=curdir,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+        time.sleep(0.1)
+
+    def tearDown(self):
+        self.proc.terminate()
+
+    async def ws_connect(self, url:str):
+        for i in range(20):
+            await asyncio.sleep(0.05)
+            try:
+                ws = await to_asyncio_future(websocket.websocket_connect(url))
+                return ws
+            except Exception:
+                continue
+            else:
+                break
+        raise OSError('connection refused to {}'.format(url))
+
+    @sync
+    async def test_autoshutdown(self):
+        await asyncio.sleep(0.1)
+        ws = await self.ws_connect('ws://'+self.addr+'/rimo_ws')
+        ws.close()
+        await asyncio.sleep(0.3)
+        self.assertIsNotNone(self.proc.poll())
+
+    @sync
+    async def test_reload(self):
+        await asyncio.sleep(0.1)
+        ws = await self.ws_connect('ws://'+self.addr+'/rimo_ws')
+        ws.close()
+        ws = await self.ws_connect('ws://'+self.addr+'/rimo_ws')
+        await asyncio.sleep(0.3)
+        self.assertIsNone(self.proc.poll())
+
+    @sync
+    async def test_multi_connection(self):
+        await asyncio.sleep(0.1)
+        ws1 = await self.ws_connect('ws://'+self.addr+'/rimo_ws')
+        ws2 = await self.ws_connect('ws://'+self.addr+'/rimo_ws')
+        ws1.close()
+        await asyncio.sleep(0.3)
+        self.assertIsNone(self.proc.poll())
+        ws2.close()
+        await asyncio.sleep(0.3)
+        self.assertIsNotNone(self.proc.poll())
+
+
+class TestAutoShutdownTornado(TestAutoShutdownAIO):
+    test_file = path.join(curdir, 'tornado_server.py')
--- wdom/tests/tornado_server.py
+++ wdom/tests/tornado_server.py
@@ -5,15 +5,17 @@ import sys
 from os.path import dirname, abspath
 import asyncio
 
+from tornado.platform.asyncio import AsyncIOMainLoop
+AsyncIOMainLoop().install()
+
 root = dirname(dirname(dirname(abspath(__file__))))
 sys.path.append(root)
 
 from wdom import options
 from wdom.document import get_document
-from wdom.server_aio import start_server, stop_server, get_app
+from wdom.server_tornado import start_server, stop_server, get_app
 
 options.parse_command_line()
-
 loop = asyncio.get_event_loop()
 app = get_app(get_document())
 server = start_server(app, loop=loop)
