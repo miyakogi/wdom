6b4b2b6db9f959ca1120b9d6d1fd96f86852a6ce
miyakogi <miyako.dev@gmail.com>
2016-04-15 06:11:38 +0900

    Improve theme loader

--- wdom/themes/__init__.py
+++ wdom/themes/__init__.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import re
 import logging
 
 from wdom import options
@@ -15,17 +16,20 @@ curdir = os.path.dirname(os.path.abspath(__file__))
 theme = options.config.theme
 if theme:
     if theme.endswith('.py'):
-        theme = theme.replace('.py', '')
-    theme_list = sorted([file.replace('.py', '')
-                         for file in os.listdir(curdir)
-                         if not file.startswith('_')])
+        theme = re.sub(r'\.py[codx]?$', '', theme)
+    theme_list = [re.sub(r'\.py[codx]?$', '', file)
+                  for file in os.listdir(curdir)
+                  if not file.startswith('_')]
     if theme in theme_list:
         # import theme
         logger.info('Use theme: {}'.format(theme))
         exec('from wdom.themes.{} import *'.format(theme))
     else:
-        logger.warn('Unknown theme "{}" was specified. Use default.'.format(theme))
-        logger.warn('Available themes: {}'.format(', '.join(theme_list)))
+        logger.warn(
+            'Unknown theme "{}" was specified. Use default.'.format(theme))
+        # Remove duplicated module name (*.py and *.pyc may exists)
+        logger.warn(
+            'Available themes: {}'.format(', '.join(sorted(set(theme_list)))))
         from wdom.tag import *
 else:
     # Use default
