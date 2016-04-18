c4859c11a155defffac3a7dd8d99db342ad397b2
miyakogi <miyako.dev@gmail.com>
2016-04-18 10:38:10 +0900

    Add extra css for pure css

--- wdom/themes/pure.py
+++ wdom/themes/pure.py
@@ -7,17 +7,17 @@ from wdom.tag import *
 
 css_files = [
     'http://yui.yahooapis.com/pure/0.6.0/pure-min.css',
+    '_static/css/pure-extra.css',
 ]
 
 Button = NewTag('Button', bases=Button, class_='pure-button')
 DefaultButton = NewTag('DefaultButton', 'button', Button)
 PrimaryButton = NewTag('PrimaryButton', 'button', Button, class_='pure-button-primary')
-SuccessButton = NewTag('SuccessButton', 'button', Button)
-InfoButton = NewTag('InfoButton', 'button', Button)
-WarningButton = NewTag('WarningButton', 'button', Button)
-DangerButton = NewTag('DangerButton', 'button', Button)
-ErrorButton = NewTag('ErrorButton', 'button', Button)
-LinkButton = NewTag('LinkButton', 'button', Button)
+SuccessButton = NewTag('SuccessButton', 'button', Button, class_='pure-button-success')
+InfoButton = NewTag('InfoButton', 'button', Button, class_='pure-button-secondary')
+WarningButton = NewTag('WarningButton', 'button', Button, class_='pure-button-warning')
+DangerButton = NewTag('DangerButton', 'button', Button, class_='pure-button-error')
+LinkButton = NewTag('LinkButton', 'button', Button, class_='pure-button-link')
 
 Form = NewTag('Form', 'form', Form, class_='pure-form')
 
