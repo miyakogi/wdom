8f4e8ac1231d694e7dc72ca77a7b85b41eab0952
miyakogi <miyako.dev@gmail.com>
2016-04-26 17:06:07 +0900

    Update test collection

--- wdom/tests/remote_browser/test_tornado.py
+++ wdom/tests/remote_browser/test_tornado.py
@@ -2,19 +2,25 @@
 # -*- coding: utf-8 -*-
 
 import os
+from itertools import chain
+
 
 from wdom import server_tornado
 from wdom.misc import install_asyncio
-from wdom.testing import TestCase
-from wdom.tests.remote_browser.test_node import WebElementTestCase, EventTestCase
-from wdom.tests.remote_browser.test_tag import NodeTestCase, InputTestCase
+from wdom.testing import TestCase, RemoteBrowserTestCase
+from wdom.tests.remote_browser import test_node, test_tag
 
 
 def setUpModule():
     install_asyncio()
 
 
-test_cases = (WebElementTestCase, EventTestCase, NodeTestCase, InputTestCase)
+test_cases = (
+    case for case in chain(vars(test_node).values(), vars(test_tag).values())
+    if isinstance(case, type) and
+    issubclass(case, RemoteBrowserTestCase) and
+    not isinstance(case, TestCase)
+)
 wait_time = 0.2 if os.environ.get('TRAVIS', False) else 0.05
 
 for case in test_cases:
