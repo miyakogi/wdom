12209289ebbdd5d7b93e6eb671582a36bec1d6c2
miyakogi <miyako.dev@gmail.com>
2017-07-23 19:39:44 +0900

    Raise warning when python version is < 3.5

--- wdom/__init__.py
+++ wdom/__init__.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+import sys
+
+if sys.version_info < (3, 5):
+    import warnings
+    warnings.warn(
+        'Next version of WDOM will not support python 3.4. Please update to version 3.6+.'  # noqa: E501
+    )
