38b6f9272a6dc84e9cd07a207560f668f7e7994d
miyakogi <miyako.dev@gmail.com>
2017-09-14 01:24:48 +0900

    Bump version: 0.2.1 → 0.3.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ tests_require = ['parameterized', 'selenium', 'syncer']
 
 setup(
     name='wdom',
-    version='0.2.1',
+    version='0.3.0',
     description='GUI library for browser-based desktop applications',
     long_description=readme,
     author='Hiroyuki Takagi',
