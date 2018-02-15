f949eb758ed3538d3af095ccc03e1055291b459b
miyakogi <miyako.dev@gmail.com>
2018-02-15 16:12:10 +0900

    Use tox on travis

--- tests/test_selenium/test_server.py
+++ tests/test_selenium/test_server.py
@@ -27,7 +27,7 @@ ENV = os.environ.copy()
 ENV['PYTHONPATH'] = ROOTDIR
 
 src_base = '''
-import sys
+import sys  # noqa: F401
 import asyncio
 
 from wdom.tag import H1
@@ -38,7 +38,7 @@ loop = asyncio.get_event_loop()
 doc = get_document()
 doc.body.appendChild(H1('FIRST', id='h1'))
 doc.add_cssfile('testdir/test.css')
-server.add_static_path('testdir', '{curdir}/testdir')
+server.add_static_path('testdir', '{curdir}/testdir')  # noqa: E501
 server.start_server(loop=loop, check_time=10)
 loop.run_forever()
 '''.format(curdir=CURDIR)
